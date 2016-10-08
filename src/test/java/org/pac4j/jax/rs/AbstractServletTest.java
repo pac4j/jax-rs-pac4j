2c9e3b786acb5877e4ff7ebf54ba9edc89f8f17e
Victor Noël <victor.noel@crazydwarves.org>
2016-10-08 19:07:36 +0200

    Add tests for servlet based JAX-RS implementations
    Tests with jersey/grizzly and resteasy/undertow

---
+++ src/test/java/org/pac4j/jax/rs/AbstractServletTest.java
@@ -0,0 +1,144 @@
+package org.pac4j.jax.rs;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Form;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.NewCookie;
+import javax.ws.rs.core.Response;
+
+import org.glassfish.grizzly.http.server.util.Globals;
+import org.glassfish.jersey.client.ClientProperties;
+import org.junit.Test;
+import org.pac4j.core.client.Clients;
+import org.pac4j.core.config.Config;
+import org.pac4j.core.profile.CommonProfile;
+import org.pac4j.http.client.indirect.FormClient;
+import org.pac4j.http.credentials.authenticator.test.SimpleTestUsernamePasswordAuthenticator;
+import org.pac4j.jax.rs.annotations.Pac4JCallback;
+import org.pac4j.jax.rs.annotations.Pac4JProfile;
+import org.pac4j.jax.rs.annotations.Pac4JSecurity;
+
+public abstract class AbstractServletTest {
+
+    @Path("/")
+    public static class TestResource {
+
+        @GET
+        @Path("no")
+        public String get() {
+            return "ok";
+        }
+
+        @GET
+        @Path("logged")
+        @Pac4JSecurity(authorizers = "isAuthenticated")
+        public String logged() {
+            return "ok";
+        }
+        
+        @GET
+        @Path("inject")
+        @Pac4JSecurity(authorizers = "isAuthenticated")
+        public String inject(@Pac4JProfile CommonProfile profile) {
+            if (profile != null) {
+                return "ok";
+            } else {
+                return "error";
+            }
+        }
+
+        @POST
+        @Path("login")
+        // TODO apparently we need to disable session renewal because grizzly
+        // send 2 JSESSIONID if not...
+        @Pac4JCallback(defaultUrl = "/logged", renewSession = false)
+        public void login() {
+
+        }
+    }
+
+    protected Config getConfig() {
+        FormClient client = new FormClient("notUsedLoginUrl",
+                new SimpleTestUsernamePasswordAuthenticator());
+        // in case of invalid credentials, we simply want the error
+        client.setAjaxRequestResolver((c) -> true);
+
+        Clients clients = new Clients("notUsedCallbackUrl", client);
+        Config config = new Config(clients);
+
+        // needed by callback since we don't specify client in the URL parameter
+        clients.setDefaultClient(client);
+        return config;
+    }
+
+    protected abstract WebTarget getTarget(String url);
+
+    @Test
+    public void testNoPac4j() {
+        final String ok = getTarget("/no").request().get(String.class);
+        assertThat(ok).isEqualTo("ok");
+    }
+
+    @Test
+    public void testNotLogged() {
+        final Response res = getTarget("/logged").request().get();
+        assertThat(res.getStatus()).isEqualTo(401);
+    }
+
+    @Test
+    public void testLogin() {
+        Form form = new Form();
+        form.param("username", "foo");
+        form.param("password", "foo");
+        final Response login = getTarget("/login").request()
+                .property(ClientProperties.FOLLOW_REDIRECTS, false)
+                .post(Entity.entity(form,
+                        MediaType.APPLICATION_FORM_URLENCODED_TYPE));
+        assertThat(login.getStatus()).isEqualTo(302);
+
+        final NewCookie cookie = login.getCookies()
+                .get(Globals.SESSION_COOKIE_NAME);
+        assertThat(cookie).isNotNull();
+
+        final String ok = getTarget("/logged").request().cookie(cookie)
+                .get(String.class);
+        assertThat(ok).isEqualTo("ok");
+    }
+    
+    @Test
+    public void testInject() {
+        Form form = new Form();
+        form.param("username", "foo");
+        form.param("password", "foo");
+        final Response login = getTarget("/login").request()
+                .property(ClientProperties.FOLLOW_REDIRECTS, false)
+                .post(Entity.entity(form,
+                        MediaType.APPLICATION_FORM_URLENCODED_TYPE));
+        assertThat(login.getStatus()).isEqualTo(302);
+
+        final NewCookie cookie = login.getCookies()
+                .get(Globals.SESSION_COOKIE_NAME);
+        assertThat(cookie).isNotNull();
+
+        final String ok = getTarget("/inject").request().cookie(cookie)
+                .get(String.class);
+        assertThat(ok).isEqualTo("ok");
+    }
+
+    @Test
+    public void testLoginFail() {
+        Form form = new Form();
+        form.param("username", "foo");
+        form.param("password", "bar");
+        final Response res = getTarget("/login").request().post(Entity
+                .entity(form, MediaType.APPLICATION_FORM_URLENCODED_TYPE));
+        assertThat(res.getStatus()).isEqualTo(403);
+
+    }
+}
---
+++ src/test/java/org/pac4j/jax/rs/JerseyGrizzlyServletTest.java
@@ -0,0 +1,71 @@
+package org.pac4j.jax.rs;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.WebTarget;
+
+import org.glassfish.jersey.client.JerseyClientBuilder;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.servlet.ServletContainer;
+import org.glassfish.jersey.test.DeploymentContext;
+import org.glassfish.jersey.test.JerseyTest;
+import org.glassfish.jersey.test.ServletDeploymentContext;
+import org.glassfish.jersey.test.TestProperties;
+import org.glassfish.jersey.test.grizzly.GrizzlyWebTestContainerFactory;
+import org.glassfish.jersey.test.spi.TestContainerException;
+import org.glassfish.jersey.test.spi.TestContainerFactory;
+import org.junit.After;
+import org.junit.Before;
+import org.pac4j.core.config.Config;
+import org.pac4j.jax.rs.features.Pac4JSecurityFeature;
+import org.pac4j.jax.rs.features.jersey.Pac4JValueFactoryProvider;
+
+public class JerseyGrizzlyServletTest extends AbstractServletTest {
+
+    private MyJerseyTest jersey;
+
+    @Before
+    public void setUp() throws Exception {
+        jersey = new MyJerseyTest();
+        jersey.setUp();
+        // let's force use a JerseyClient!
+        jersey.setClient(new JerseyClientBuilder().build());
+    }
+
+    public class MyJerseyTest extends JerseyTest {
+
+        @Override
+        protected TestContainerFactory getTestContainerFactory()
+                throws TestContainerException {
+            return new GrizzlyWebTestContainerFactory();
+        }
+
+        @Override
+        protected DeploymentContext configureDeployment() {
+            forceSet(TestProperties.CONTAINER_PORT, "0");
+            
+            Config config = getConfig();
+            ResourceConfig app = new ResourceConfig(TestResource.class)
+                    .register(new Pac4JSecurityFeature(config))
+                    .register(new Pac4JValueFactoryProvider.Binder(config));
+
+            return ServletDeploymentContext
+                    .forServlet(new ServletContainer(app)).build();
+        }
+
+        @SuppressWarnings("UselessOverridingMethod")
+        @Override
+        protected Client setClient(Client client) {
+            return super.setClient(client);
+        }
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        jersey.tearDown();
+    }
+
+    @Override
+    protected WebTarget getTarget(String url) {
+        return jersey.target(url);
+    }
+}
