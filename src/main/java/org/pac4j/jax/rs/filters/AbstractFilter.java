7d119da6a6611d00334e12ccc479d2b49bb981a6
Victor Noël <victor.noel@crazydwarves.org>
2016-11-06 15:14:06 +0100

    Fixes a PMD warning

--- src/main/java/org/pac4j/jax/rs/filters/AbstractFilter.java
+++ src/main/java/org/pac4j/jax/rs/filters/AbstractFilter.java
@@ -2,8 +2,6 @@ package org.pac4j.jax.rs.filters;
 
 import java.io.IOException;
 
-import javax.annotation.Priority;
-import javax.ws.rs.Priorities;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.core.MediaType;
