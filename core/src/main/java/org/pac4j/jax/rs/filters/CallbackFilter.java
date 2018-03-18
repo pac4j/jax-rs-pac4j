3e846c371c9b9908f522fd529057bb168d69f6bc
Victor Noël <victor.noel@crazydwarves.org>
2018-03-18 14:22:56 +0100

    Use a multimodule maven organisation for 2.x
     - Bump to pac4j 2.3.0
     - Users should depend on jersey-pac4j or resteasy-pac4j
     - Users can still depend on jax-rs-pac4j for backward compatibility
     - Add saveInSession parameter for callback endpoints

---
+++ core/src/main/java/org/pac4j/jax/rs/filters/CallbackFilter.java
@@ -0,0 +1,105 @@
+package org.pac4j.jax.rs.filters;
+
+import java.io.IOException;
+
+import javax.annotation.Priority;
+import javax.ws.rs.Priorities;
+import javax.ws.rs.ext.Providers;
+
+import org.pac4j.core.config.Config;
+import org.pac4j.core.engine.CallbackLogic;
+import org.pac4j.core.engine.DefaultCallbackLogic;
+import org.pac4j.jax.rs.pac4j.JaxRsContext;
+import org.pac4j.jax.rs.pac4j.JaxRsProfileManager;
+
+/**
+ * 
+ * @author Victor Noel - Linagora
+ * @since 1.0.0
+ *
+ */
+@Priority(Priorities.AUTHORIZATION)
+public class CallbackFilter extends AbstractFilter {
+
+    private CallbackLogic<Object, JaxRsContext> callbackLogic;
+
+    private String defaultUrl;
+
+    private Boolean saveInSession;
+
+    private Boolean multiProfile;
+
+    private Boolean renewSession;
+
+    public CallbackFilter(Providers providers) {
+        super(providers);
+    }
+
+    @Override
+    protected void filter(JaxRsContext context) throws IOException {
+        Config config = getConfig();
+
+        CallbackLogic<Object, JaxRsContext> logic = buildLogic(config);
+
+        // TODO remove the null check once pac4j 2.3.1 is released
+        // cf https://github.com/pac4j/pac4j/pull/1121
+        if (saveInSession != null && logic instanceof DefaultCallbackLogic) {
+            ((DefaultCallbackLogic) logic).setSaveInSession(saveInSession);
+        }
+
+        logic.perform(context, config, adapter(config), context.getAbsolutePath(defaultUrl, false),
+                multiProfile, renewSession);
+    }
+
+    protected CallbackLogic<Object, JaxRsContext> buildLogic(Config config) {
+        if (callbackLogic != null) {
+            return callbackLogic;
+        } else if (config.getCallbackLogic() != null) {
+            return config.getCallbackLogic();
+        } else {
+            DefaultCallbackLogic<Object, JaxRsContext> logic = new DefaultCallbackLogic<>();
+            logic.setProfileManagerFactory(JaxRsProfileManager::new);
+            return logic;
+        }
+    }
+
+    public CallbackLogic<Object, JaxRsContext> getCallbackLogic() {
+        return callbackLogic;
+    }
+
+    public void setCallbackLogic(CallbackLogic<Object, JaxRsContext> callbackLogic) {
+        this.callbackLogic = callbackLogic;
+    }
+
+    public String getDefaultUrl() {
+        return defaultUrl;
+    }
+
+    public void setDefaultUrl(String defaultUrl) {
+        this.defaultUrl = defaultUrl;
+    }
+
+    public boolean isSaveInSession() {
+        return saveInSession;
+    }
+
+    public void setSaveInSession(Boolean saveInSession) {
+        this.saveInSession = saveInSession;
+    }
+
+    public boolean isMultiProfile() {
+        return multiProfile;
+    }
+
+    public void setMultiProfile(Boolean multiProfile) {
+        this.multiProfile = multiProfile;
+    }
+
+    public boolean isRenewSession() {
+        return renewSession;
+    }
+
+    public void setRenewSession(Boolean renewSession) {
+        this.renewSession = renewSession;
+    }
+}
