d7e65d0a1945e524520b26caaee6d6058959c4d8
Jean Britz <britz.jean@gmail.com>
2025-03-07 07:13:51 -0500

    Added javadoc comments for DefaultJaxRsWebContextFactory and ServletSessionStoreFactory

--- core/src/main/java/org/pac4j/jax/rs/pac4j/DefaultJaxRsWebContextFactory.java
+++ core/src/main/java/org/pac4j/jax/rs/pac4j/DefaultJaxRsWebContextFactory.java
@@ -1,5 +1,6 @@
 package org.pac4j.jax.rs.pac4j;
 
+import jakarta.ws.rs.container.ContainerRequestContext;
 import org.pac4j.core.context.FrameworkParameters;
 import org.pac4j.core.context.WebContext;
 import org.pac4j.core.context.WebContextFactory;
@@ -7,10 +8,33 @@ import org.pac4j.core.exception.TechnicalException;
 import org.pac4j.jax.rs.features.JaxRsContextFactoryProvider;
 import org.pac4j.jax.rs.helpers.ProvidersContext;
 
+/**
+ * Default implementation of {@link WebContextFactory} for JAX-RS environments.
+ * <p>
+ *  This factory creates a {@link WebContext} from JAX-RS framework parameters.
+ *  It expects the provided {@link FrameworkParameters} to be an instance of
+ *  {@link JaxRsFrameworkParameters} in order to extract the necessary context.
+ * </p>
+ *
+ * @since 7.0.0
+ */
 public class DefaultJaxRsWebContextFactory implements WebContextFactory {
 
     public static final WebContextFactory INSTANCE = new DefaultJaxRsWebContextFactory();
 
+    /**
+     * Creates a new {@link WebContext} using the provided framework parameters.
+     * <p>
+     * The creation of the context is done by resolving the {@link JaxRsContextFactoryProvider.JaxRsContextFactory}
+     * from the current {@link ProvidersContext}. The {@link JaxRsContextFactoryProvider.JaxRsContextFactory} provides
+     * a new {@link WebContext} based on the {@link ContainerRequestContext} contained in the
+     * {@link JaxRsFrameworkParameters}.
+     * </p>
+     *
+     * @param parameters framework parameters used to create the {@link WebContext}
+     * @return New instance of {@link WebContext}
+     * @throws TechnicalException if the provided parameters are not an instance of {@link JaxRsFrameworkParameters}
+     */
     @Override
     public WebContext newContext(FrameworkParameters parameters) {
         if(parameters instanceof JaxRsFrameworkParameters jaxRsFrameworkParameters) {
--- core/src/main/java/org/pac4j/jax/rs/servlet/pac4j/ServletSessionStoreFactory.java
+++ core/src/main/java/org/pac4j/jax/rs/servlet/pac4j/ServletSessionStoreFactory.java
@@ -4,12 +4,28 @@ import org.pac4j.core.context.FrameworkParameters;
 import org.pac4j.core.context.session.SessionStore;
 import org.pac4j.core.context.session.SessionStoreFactory;
 
+/**
+ * {@link SessionStoreFactory} implementation that provides a singleton instance of
+ * a {@link ServletSessionStore} for servlet-based environments.
+ * <p>
+ *  This factory returns the same {@link ServletSessionStore} instance for all calls
+ *  to {@link ServletSessionStoreFactory#newSessionStore}, effectively sharing the session store across the application.
+ * </p>
+ *
+ * @since 7.0.0
+ */
 public class ServletSessionStoreFactory implements SessionStoreFactory {
 
     public static final SessionStoreFactory INSTANCE = new ServletSessionStoreFactory();
 
     private static final SessionStore SERVLET_SESSION_STORE = new ServletSessionStore();
 
+    /**
+     * Returns the shared {@link ServletSessionStore} instance.
+     *
+     * @param parameters the framework parameters (not used in this implementation)
+     * @return the singleton {@link ServletSessionStore} instance
+     */
     @Override
     public SessionStore newSessionStore(FrameworkParameters parameters) {
         return SERVLET_SESSION_STORE;
