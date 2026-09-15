916a851d2e91b5a44a221094147f678d65917487
christian0101 <christian139601@gmail.com>
2026-09-15 13:38:24 +0200

    Add more unit tests to enforce the expected behaviour

--- core/src/test/java/org/pac4j/jax/rs/features/JaxRsSessionStoreProviderTest.java
+++ core/src/test/java/org/pac4j/jax/rs/features/JaxRsSessionStoreProviderTest.java
@@ -1,10 +1,10 @@
-package org.pac4j.jax.rs.pac4j;
+package org.pac4j.jax.rs.features;
 
 import org.junit.Test;
 import org.pac4j.core.config.Config;
 import org.pac4j.core.context.session.SessionStore;
 import org.pac4j.core.exception.TechnicalException;
-import org.pac4j.jax.rs.features.JaxRsSessionStoreProvider;
+import org.pac4j.jax.rs.pac4j.NoOpSessionStoreFactory;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
@@ -14,7 +14,7 @@ public class JaxRsSessionStoreProviderTest {
     @Test
     public void should_provide_session_store_when_session_store_factory_is_set() {
         Config config = new Config();
-        config.setSessionStoreFactory(new NoOpSessionStoreFactory());
+        config.setSessionStoreFactory(NoOpSessionStoreFactory.INSTANCE);
 
         JaxRsSessionStoreProvider jaxRsSessionStoreProvider = new JaxRsSessionStoreProvider(config);
         SessionStore sessionStore = jaxRsSessionStoreProvider.getContext(null);
